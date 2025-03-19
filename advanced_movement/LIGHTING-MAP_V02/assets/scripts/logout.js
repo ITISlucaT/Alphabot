5848cef4c0227e7f66d7296d44785ee270ee5cb1
Luca Torelli <luca.torelli@itiscuneo.eu>
2025-03-19 23:02:34 +0100

    modified:   .gitignore

--- advanced_movement/LIGHTING-MAP_V02/assets/scripts/logout.js
+++
@@ -1,6 +0,0 @@
-const btnLogout = document.getElementById('btnLogout');
-
-btnLogout.addEventListener('click', () => {
-    localStorage.removeItem('userData');
-    window.location.href = '/LIGHTING-MAP/index.html';
-})
